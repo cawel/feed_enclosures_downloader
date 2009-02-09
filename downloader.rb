8b8c5c703175b924fcafd7cacd802dbbfac2a4d3
Martin Carel <cawel00@yahoo.com>
2009-02-09 11:20:19 -0500

    adding files to repo

---
+++ downloader.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'simple-rss'
+require 'open-uri'
+
+FEEDS_DOWNLOAD_DIR = 'downloads'
+RSS_LINKS_FILENAME = 'rss_links'
+CWD = File.expand_path(File.dirname(__FILE__))
+
+def download_enclosure enclosure, description
+	filename = File.basename(enclosure)
+	output_dir = File.join(CWD, FEEDS_DOWNLOAD_DIR, description)
+	FileUtils.chdir output_dir
+	FileUtils.mkdir_p output_dir
+		`wget #{enclosure}` unless File.exists?(File.join(output_dir, filename))
+end
+
+File.open(RSS_LINKS_FILENAME) do |file|
+	while(rss_link = file.gets) do
+		feed = SimpleRSS.parse open(rss_link)
+		enclosure = feed.items.first.guid
+		download_enclosure(enclosure, feed.description)
+	end
+end
